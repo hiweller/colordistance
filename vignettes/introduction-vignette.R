d5f1cfd51e5f9d8503c0f41f02a1862856d4b31a
Hannah Weller <hannah@Problematicus.local>
2017-05-17 13:54:14 -0500

    Built vignettes

--- vignettes/introduction-vignette.R
+++
@@ -1,9 +0,0 @@
-## ---- echo=F, fig.align="center", fig.cap="Eight different species (yes, really!) of *Heliconius* butterflies; from @Meyer2006.", out.width="500px"----
-knitr::include_graphics("Heliconius_mimicry.jpg")
-
-## ---- echo=F, fig.align="center", fig.cap="Separating each butterfly into its own image results in 8 images.", out.width="500px"----
-knitr::include_graphics("Heliconius_divided.jpg")
-
-## ---- echo=F, fig.align="center", fig.cap="The pin is small, but since it's not a part of the butterfly, we should exclude it.", out.width="350px"----
-knitr::include_graphics("Heliconius_08_edit.jpg")
-
